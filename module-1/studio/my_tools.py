ff907b2db89a55d40904bf4b70b1008a972874cb
kimHako <l36h7129@gmail.com>
2024-12-03 15:18:37 +0900

    feat: 첫 노드에서 특정 조건에 따라 라우팅 하기

--- module-1/studio/my_tools.py
+++ module-1/studio/my_tools.py
@@ -1,3 +1,4 @@
+from typing import Literal
 from typing_extensions import TypedDict
 from langgraph.graph import StateGraph, START, END
 
@@ -8,15 +9,25 @@ class State(TypedDict):
 # node 정의
 def node_A(state):
     print('--node A--')
-    return {"graph_state": state['graph_state'] + " First!"}
+    return {"graph_state": state['graph_state']}
 
 def node_B(state):
     print('--node B--')
-    return {"graph_state": state['graph_state'] + " Second!"}
+    return {"graph_state": state['graph_state'] + " B!"}
 
 def node_C(state):
     print('--node C--')
-    return {"graph_state": state['graph_state'] + " Third!"}
+    return {"graph_state": state['graph_state'] + " C!"}
+
+
+def decide_next_A(state) -> Literal["node_B", "node_C"]:
+    user_message = state['graph_state']
+    print('길이: ', len(user_message))
+    if len(user_message)%2 == 0:
+        return "node_B"
+    else:
+        return "node_C"
+
 
 # graph 정의
 builder = StateGraph(State)
@@ -26,10 +37,10 @@ builder.add_node("node_C", node_C)
 
 # node 연결
 builder.add_edge(START, "node_A")
-builder.add_edge("node_A", "node_B")
-builder.add_edge("node_A", "node_C")
-builder.add_edge("node_B", "node_C")
+builder.add_conditional_edges("node_A", decide_next_A)
+builder.add_edge("node_B", END)
 builder.add_edge("node_C", END)
 
 # graph 생성
 graph = builder.compile();
+graph.invoke({"graph_state":"IamHako"})
