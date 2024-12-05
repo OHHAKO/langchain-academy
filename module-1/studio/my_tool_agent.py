1118876943c655d5e3d4129c14a8a86cb60c98fe
kimHako <l36h7129@gmail.com>
2024-12-06 08:46:04 +0900

    feat: 도구를 세 가지로 만들어 LLM에게 주기

--- module-1/studio/my_tool_agent.py
+++ module-1/studio/my_tool_agent.py
@@ -1,3 +1,4 @@
+import random
 from typing import Literal, Annotated
 from typing_extensions import TypedDict
 from langgraph.graph import StateGraph, START, END
@@ -8,18 +9,42 @@ from langgraph.graph import MessagesState
 from langgraph.prebuilt import ToolNode
 
 # 도구 정의
+def random_number(a: int, b: int) -> int:
+    """Returns a random integer between a and b.
+
+    Args:
+        a: first int
+        b: second int
+    """
+    print('[도구 호출] random_number: ', a, b)
+    return random.randint(a, b)
+
 def is_odd_number(a: int) -> bool:
     """Returns True if a is odd, False otherwise.
 
     Args:
         a: int
     """
-    print('도구 호출: ', a)
+    print('[도구 호출] is_odd_number: ', a)
     return a % 2 == 1
+
+def is_prime_number(a: int) -> bool:
+    """Returns True if a is prime, False otherwise.
+
+    Args:
+        a: first int
+    """
+    print('[도구 호출] is_prime_number: ', a)
+    if a < 2:
+        return False
+    for i in range(2, int(a**0.5) + 1):
+        if a % i == 0:
+            return False
+    return True
 
 # LLM에게 도구 쥐어주기
 llm = ChatOpenAI(model="gpt-4o")
-llm_with_tools = llm.bind_tools([is_odd_number],parallel_tool_calls=True)
+llm_with_tools = llm.bind_tools([is_odd_number, random_number, is_prime_number],parallel_tool_calls=True)
 
 
 # state 정의
@@ -50,7 +75,7 @@ def tools_condition(state) -> Literal[END, 'tool']:
 # graph 정의
 builder = StateGraph(MessageState)
 builder.add_node("assistant", assistant_node)
-builder.add_node("tool", ToolNode([is_odd_number]))
+builder.add_node("tool", ToolNode([is_odd_number, random_number, is_prime_number]))
 
 # node 연결
 builder.add_edge(START, "assistant")
